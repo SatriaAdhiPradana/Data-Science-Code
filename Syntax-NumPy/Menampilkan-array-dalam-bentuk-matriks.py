840a974f7778fa63b417ef00180a4d56e59c42f5
Satria Adhi Pradana <92844616+SatriaAdhiPradana@users.noreply.github.com>
2023-02-17 13:13:23 +0700

    Create Menampilkan-array-dalam-bentuk-matriks.py

---
+++ Syntax-NumPy/Menampilkan-array-dalam-bentuk-matriks.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Menampilkan array dalam bentuk matriks
+mat = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(mat)
+
+# Menampilkan elemen baris dan kolom tertentu
+print(mat[0, 0])
+print(mat[1, 2])
